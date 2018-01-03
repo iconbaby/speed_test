22c8c6123e36609e430e3a8317654f92424a5ed7
Your Name <644187060@qq.com>
2018-01-03 14:41:35 +0800

    complete randomAccessFile DEMO

--- randomaccessfiledemo/src/main/java/com/hg/randomaccessfiledemo/MainActivity.java
+++ randomaccessfiledemo/src/main/java/com/hg/randomaccessfiledemo/MainActivity.java
@@ -1,13 +1,47 @@
 package com.hg.randomaccessfiledemo;
 
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.RandomAccessFile;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new Thread() {
+            @Override
+            public void run() {
+                String filesDir = getFilesDir().toString();
+                File file = new File(filesDir + File.separator + "tst.txt");
+                Log.i(TAG, "run: " + filesDir);
+                if (file.exists()) {
+                    file.delete();
+                }
+                try {
+                    file.createNewFile();
+                    RandomAccessFile raf = new RandomAccessFile(file, "rw");
+                    raf.write("abcde".getBytes());
+                    raf.seek(3);
+                    int read = raf.read();
+                    Log.i(TAG, "run: " + (char) read);
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+
+            }
+        }.start();
     }
 }
