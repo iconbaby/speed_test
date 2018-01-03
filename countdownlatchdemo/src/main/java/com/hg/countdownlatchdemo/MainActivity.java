d5184b888f04acf91b107717e1f2f98851c8dc00
Your Name <644187060@qq.com>
2018-01-03 13:54:38 +0800

    complete countDownLatch demo

--- countdownlatchdemo/src/main/java/com/hg/countdownlatchdemo/MainActivity.java
+++ countdownlatchdemo/src/main/java/com/hg/countdownlatchdemo/MainActivity.java
@@ -2,12 +2,47 @@ package com.hg.countdownlatchdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.concurrent.CountDownLatch;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        CountDownLatch countDownLatch = new CountDownLatch(2);
+        new WorkThread("thread1", countDownLatch).start();
+        new WorkThread("thread2", countDownLatch).start();
+        try {
+            countDownLatch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        Log.i(TAG, "work end");
+    }
+
+    class WorkThread extends Thread {
+        private String threadName;
+        private CountDownLatch countDownLatch;
+
+        public WorkThread(String name, CountDownLatch countDownLatch) {
+            this.threadName = name;
+            this.countDownLatch = countDownLatch;
+        }
+
+        @Override
+        public void run() {
+            Log.i(TAG, "thread : " + threadName + "start run");
+            try {
+                Thread.sleep(3000);
+                countDownLatch.countDown();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        }
     }
 }
